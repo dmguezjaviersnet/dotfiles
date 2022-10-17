23c60b363ad0e516d2bef971714b2c4144159899
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2022-10-17 11:08:05 -0400

    Saving awesome rxhyn's rice

---
+++ .config/awesome/configuration/apps.lua
@@ -0,0 +1,41 @@
+local filesystem = require("gears.filesystem")
+local config_dir = filesystem.get_configuration_dir()
+local utils_dir = config_dir .. "utilities/"
+
+return {
+	--- Default Applications
+	default = {
+		--- Default terminal emulator
+		terminal = "alacritty",
+		--- Default terminal emulator + tmux
+		terminal_tmux = "alacritty -e tmux",
+		--- Default music client
+		music_player = "wezterm start --class music ncmpcpp",
+		--- Default text editor
+		text_editor = "alacritty -e nvim",
+		--- Default code editor
+		code_editor = "code",
+		--- Default web browser
+		web_browser = "google-chrome-stable",
+		--- Default file manager
+		file_manager = "thunar",
+		--- Default network manager
+		network_manager = "alacritty -e nmtui",
+		--- Default bluetooth manager
+		bluetooth_manager = "blueman-manager",
+		--- Default power manager
+		power_manager = "xfce4-power-manager",
+		--- Default rofi global menu
+		app_launcher = "dmenu_run -c -l 20",
+	},
+
+	--- List of binaries/shell scripts that will execute for a certain task
+	utils = {
+		--- Fullscreen screenshot
+		full_screenshot = "flameshot gui",
+		--- Area screenshot
+		area_screenshot = utils_dir .. "flameshot gui",
+		--- Color Picker
+		color_picker = utils_dir .. "xcolor-pick",
+	},
+}
---
+++ .config/awesome/user_variables.lua
@@ -0,0 +1,18 @@
+return {
+	widget = {
+		weather = {
+			--- API Key
+			key = "98326772acdc9c87935f2c3b1afcc432",
+			--- Coordinates
+			coordinates = {
+				"23.162821", --- lat
+				"-82.302369", --- lon
+			},
+		},
+
+		--- Github activity
+		github = {
+			username = "dmguezjaviersnet",
+		},
+	},
+}
